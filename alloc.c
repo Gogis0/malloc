5bbd04e2dde9d3f4ebadb86f0e483c8d3efaaab0
Gogis0 <adriangoga0@gmail.com>
2017-04-15 22:31:10 +0200

    Committing this stub just in case

--- alloc.c
+++ alloc.c
@@ -1,4 +1,6 @@
 #include "wrapper.h"
+#include <stdio.h>
+#include <string.h>
 
 /* Kod funkcii my_alloc a my_free nahradte vlastnym. Nepouzivajte ziadne
  * globalne ani staticke premenne; jedina globalna pamat je dostupna pomocou
@@ -15,6 +17,38 @@
  * zbehnut moze a uvolni pamat.
  */
 
+typedef struct block_info {
+    int prev;
+    int next;
+    int size; /* velkost pamate bloku */
+    int addr; /* zaciatok pamate bloku */
+    int free; /* info ci je block volny */
+} preamble;
+
+
+preamble read_preamble(int pos) {
+    int p_size = sizeof(preamble);
+    char buffer[p_size];
+    preamble p;
+
+    for (int i = pos; i < pos + p_size; i++) {
+        buffer[i - pos] = mread(i);
+    }
+    memcpy(&p, buffer, p_size);
+    return p;
+}
+
+
+void write_preamble(preamble p) {
+    int p_size = sizeof(preamble);
+    char buffer[p_size];
+    memcpy(buffer, &p, p_size);
+
+    for (int i = p.addr - p_size; i < p.addr; i++) {
+        mwrite(i, buffer[i - (p.addr - p_size)]);
+    }
+}
+
 
 /**
  * Inicializacia pamate
@@ -23,7 +57,29 @@
  * na 0.
  */
 void my_init(void) {
-	return;
+    int p_size = sizeof(preamble);
+    int m_size = msize();
+
+    /* zaciatok */
+    preamble init;
+    init.addr = p_size;
+    init.size = m_size - 2*p_size;
+    init.free = 1;
+    init.next = m_size - p_size;
+    init.prev = -1;
+
+    /* koniec */
+    preamble term;
+    term.addr = m_size;
+    term.size = 0;
+    term.next = -1;
+    term.prev = 0;
+    term.free = 0;
+
+    /* zapisem mantinely do pamate */
+    write_preamble(init);
+    write_preamble(term);
+    fprintf(stderr, "%s\n", "INITIALIZATION FINISHED");
 }
 
 /**
@@ -34,20 +90,50 @@ void my_init(void) {
  * vracia FAIL.
  */
 int my_alloc(unsigned int size) {
+    int p_size = sizeof(preamble); 
+    int m_size = msize();
+    char buffer[p_size];
 
-	/* Nemozeme alokovat viac pamate, ako je dostupne */
-	if (size >= msize() - 1)
-		return FAIL;
+    /* ak ziadam vacsiu pamat ako mam dokopy */
+    if (size > m_size - p_size) return FAIL;
 
-	/* Pamat uz bola alokovana */
-	if (mread(0) == 1)
-		return FAIL;
+    preamble act_block;
+    int offset = 0, found_suitable_block = 0;
+    do {
+        /* nacitam preambulu */
+        act_block = read_preamble(offset);
+        /* fprintf(stderr, "%d\n", act_block.addr); */
 
-	/* Vsetko je OK, mozeme splnit poziadavku. Do 0teho bajtu si poznacime, ze
-	 * pamat je obsadena a vratime adresu prveho bajtu novo alokovanej pamate
-	 */
-	mwrite(0, 1);
-	return 1;
+        if (act_block.free && act_block.size >= size) {
+            found_suitable_block = 1;
+            break;
+        }
+        offset = act_block.next;
+    } while (act_block.next != -1);
+
+    /* ak som nenasiel vhodny blok, vratim FAIL */
+    if (!found_suitable_block) return FAIL;
+
+    /* nasiel som vhodne miesto, obsadim z neho kolko potrebujem */
+
+    /* ak mi ostalo dost pamate vytvorim doplnujuci block */
+    if (act_block.size - size > p_size) {
+        preamble new_block;
+        new_block.free = 1;
+        new_block.prev = act_block.addr - p_size;
+        new_block.next = act_block.next;
+        new_block.addr = act_block.addr + size + p_size;
+        write_preamble(new_block);
+    }
+   
+    /* useknem stary block */
+    act_block.free = 0;
+    act_block.size = size;
+    act_block.next = act_block.addr + size;
+    write_preamble(act_block);
+
+    /* vratim zaciatok alokovanej pamate */
+    return act_block.addr;
 }
 
 /**
@@ -59,16 +145,39 @@ int my_alloc(unsigned int size) {
  */
 
 int my_free(unsigned int addr) {
+    int p_size = sizeof(preamble);
+    char buffer[p_size];
 
-	/* Adresa nie je platnym smernikom, ktory mohol vratit my_alloc */
-	if (addr != 1)
-		return FAIL;
+    preamble act_block = read_preamble(addr - p_size);
+    act_block.free = 1;
 
-	/* Nie je alokovana ziadna pamat, nemozeme ju teda uvolnit */
-	if (mread(0) != 1)
-		return FAIL;
+    /* ak je predchadzajuci block free, mergni */
+    if (act_block.prev != -1) {
+        preamble prev_block = read_preamble(act_block.prev);
+        if (prev_block.free) {
+            prev_block.next = act_block.next;
+            prev_block.size += act_block.size + p_size;
 
-	/* Vsetko je OK, mozeme uvolnit pamat */
-	mwrite(0, 0);
-	return OK;
+            act_block = prev_block;
+
+            preamble next_block = read_preamble(act_block.next);
+            next_block.prev = act_block.addr - p_size;
+            write_preamble(next_block);
+        }
+    }
+
+    /* ak je nasledujuci block free, mergni */
+    if (act_block.next != -1) {
+        preamble next_block = read_preamble(act_block.next);
+        if (next_block.free) {
+            act_block.next = next_block.next;
+            act_block.size += next_block.size + p_size;
+
+            preamble after_next = read_preamble(next_block.next);
+            after_next.prev = next_block.prev;
+        }
+    }
+
+    /* zapis upraveny usek */
+    write_preamble(act_block);
 }
